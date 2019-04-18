fb9da0eaa33467a181d350012cb15b8ac9286c5f
poc <hub@poc.com>
2019-04-18 17:14:26 +0200

    [+](): first push can drag the cube and scale up on click

---
+++ Assets/scripts/CheckInputs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using HoloToolkit.Unity.InputModule;
+
+public class CheckInputs : MonoBehaviour, IInputHandler
+{
+    public float timeToLongPress = 1.0f;
+    private float timeClicked = 0.0f;
+
+    public virtual void OnSimpleClick() { }
+    public virtual void OnLongClick() { }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        timeClicked = Time.time;
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        if (Time.time - timeClicked < timeToLongPress)
+        {
+            Debug.Log("Short");
+            OnSimpleClick();
+        }
+        else
+        {
+            Debug.Log("Long");
+            OnLongClick();
+        }
+    }
+
+    
+}
---
+++ Assets/scripts/GestureHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.Events;
+
+public class GestureHandler : MonoBehaviour, IFocusable, IInputClickHandler, IInputHandler
+{
+
+    private bool isActive = false;
+    private GestureRecognizer recognizer;
+
+    private void Start()
+    {
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.TappedEvent += GestureRecognizer_TappedEvent;
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnFocusEnter()
+    {
+        Debug.Log("FOCUS");
+    }
+
+    public void OnFocusExit()
+    {
+        Debug.Log("NOT FOCUS");
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        transform.localScale *= 1.05f;
+    }
+
+    void OnAirTapped()
+    {
+
+        isActive = !isActive;
+    }
+
+    private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        Debug.DrawLine(headRay.origin, headRay.direction * 1000, Color.green);
+        OnAirTapped();
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        Debug.Log("PRIS");
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        Debug.Log("LACHE");
+    }
+}
+ 
